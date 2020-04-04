069cae1f9b1111b25680c7113105779705e14bf0
KelianB <kelian.baert@gmail.com>
2020-04-04 11:52:19 +0200

    improved roulette wheel performance

--- src/utils/RouletteWheel.java
+++ src/utils/RouletteWheel.java
@@ -1,8 +1,6 @@
 package utils;
 
-import java.util.NavigableMap;
 import java.util.Random;
-import java.util.TreeMap;
 
 public class RouletteWheel {
     // Build a vector of cumulative fitnesses (allows for O(log2(n)) roulette wheel selection)
@@ -20,12 +18,11 @@ public class RouletteWheel {
 
 	/** Uses binary search to achieve O(log2(n)) time complexity **/
 	public static int spin(Random rand, float[] cumulativeWeights) {
-		int length = cumulativeWeights.length - 1;
-		float sumOfWeights = cumulativeWeights[length];
+		float sumOfWeights = cumulativeWeights[cumulativeWeights.length - 1];
 		float r = sumOfWeights * rand.nextFloat();
 		
 		// Look for the index of the entry just above r
-		int a = 0, b = length - 1;
+		int a = 0, b = cumulativeWeights.length - 1;
 	    while(b-a > 1) {
 	        int mid = (a + b) / 2;
 	        if(cumulativeWeights[mid] > r)
@@ -37,34 +34,38 @@ public class RouletteWheel {
 	    return a;	
 	}
 	
+	/**
+	 * Spin a single roulette wheel (weighted choice) with the given probabilities.
+	 * @param rand - A random generator
+	 * @param probabilities - An array of probabilities (or weights)
+	 * @return an index between 0 (inclusive) and the length of the probabilities array (exclusive)
+	 */
 	public static int spinOnce(Random rand, float[] probabilities) { 
-		//return spin(rand, createCumulativeWeights(probabilities));
-		return spin(rand, createMap(probabilities));
+		if(probabilities.length == 1)
+			return 0;
+		
+		return spin(rand, createCumulativeWeights(probabilities));
 	}
 	
+	/* Test of Roulette Wheel implementation */
 	/*public static void main(String[] args) {
-		float[] probas = new float[] {1.0f, 1.0f, 1.0f};
-		for(int i = 0; i < 10; i++)
-			System.out.println(spinOnce(new Random(), probas));
+		Random r = new Random();
+		
+		float[] probas = new float[] {0.5f, 0.5f, 10.0f};
+		float[] weights = createCumulativeWeights(probas);
+		
+		HashMap<Integer, Integer> outputs = new HashMap<Integer, Integer>();
+		for(int i = 0; i < probas.length; i++)
+			outputs.put(i, 0);
+		
+		int iters = 1000;
+		
+		for(int i = 0; i < iters; i++) {
+			int output = spin(r, weights);
+			outputs.put(output, outputs.get(output) + 1);
+		}
+		
+		for(int i = 0; i < probas.length; i++)
+			System.out.println("Output " + i + ": " + 100.0f * outputs.get(i) / iters + "%");
 	}*/
-	
-    public static NavigableMap<Float, Integer> createMap(float[] probabilities) {
-    	NavigableMap<Float, Integer> map = new TreeMap<Float, Integer>();
-        float total = 0;
-        for(int i = 0; i < probabilities.length; i++) {
-        	if(probabilities[i] != 0) {
-        		total += probabilities[i];
-        		map.put(total, i);
-        	}
-        }
-        return map;
-    }
-
-    public static int spin(Random rand, NavigableMap<Float, Integer> map) {
-    	if(map.size() == 1)
-    		return map.firstEntry().getValue();
-    	float total = map.lastKey();
-        float value = rand.nextFloat() * total;
-        return map.higherEntry(value).getValue();
-    }
 }
